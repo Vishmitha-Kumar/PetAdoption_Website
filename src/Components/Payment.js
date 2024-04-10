4c11e3f54255c886d90bea65c6b313ee22997b0f
Vishmitha-Kumar <vishmithak05@gmail.com>
2024-04-10 07:43:45 +0530

    payment

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -1,7 +1,100 @@
-import React from "react";
+import React ,{useState}from "react";
 import '../Assets/Css1/Payment.css';
+import {useNavigate} from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import{ToastContainer,toast} from 'react-toastify';
+import axios from 'axios'
+
 
 function Payment() {
+
+    const navJ = useNavigate();
+
+    const[data,setData]=useState({
+      name:'',
+      email:'',
+      phone:'',
+      address:'',
+      breed:'',
+      gender:'',
+      color:'',
+      paymentmethod:''
+
+  
+    })
+  
+  
+    const[confirmPassword,setConfirmPassword]=useState('');
+  
+    const handleChange=(e)=>{
+      setData({...data,[e.target.id]:e.target.value})
+    }
+  
+    const handleConfirmPasswordChange=(e)=>{
+      setConfirmPassword(e.target.value);
+    }
+  
+ 
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      
+      if (data.password === confirmPassword) {
+        
+        axios.post('http://localhost:8080/api/v1/user/save',data)
+        .then(response=>{
+          console.log("Success"+response);
+  
+          navJ('/login')
+          setData({});
+          setConfirmPassword("");
+        })
+  
+          .catch(error => {
+            toast.error('Email already registered!', {
+                position: "bottom-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+  
+        });
+        
+      } else {
+        toast.error('Invaild Username (or) Password', {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          });
+          setData({});
+          setConfirmPassword("");
+      }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         <>
             <div className="wraper">
@@ -42,17 +135,17 @@ function Payment() {
                         </div>
                         <div className="form-group">
                             <label htmlFor="payment-method">Payment Method:</label>
-                            <select id="payment-method" name="payment-method" required>
+                            <select id="paymentmethod" name="payment-method" required>
                                 <option value="">Select Payment Method</option>
-                                <option value="online">Pay Online</option>
+        
                                 <option value="cod">Cash on Delivery</option>
                             </select>
                         </div>
-                        <div className="form-group">
+                        {/* <div className="form-group">
                             <label htmlFor="amount">Amount ($):</label>
                             <input type="number" id="amount" name="amount" required min="0" />
-                        </div>
-                        <button type="submit">Submit Payment</button>
+                        </div> */}
+                        <button type="submit">Submit</button>
                     </form>
                 </div>
             </div>
